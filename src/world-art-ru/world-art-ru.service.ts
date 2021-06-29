411af7c821c901e8f9dc880c298122d743244559
Nazar Bashinskiy <nazar_bashinskiy@mail.ru>
2021-06-29 17:50:07 +0300

    fixed getCinemaListPage

--- src/world-art-ru/world-art-ru.service.ts
+++ src/world-art-ru/world-art-ru.service.ts
@@ -1,7 +1,7 @@
 import { Request } from '../common/libs';
 
 async function getCinemaListPage (page: number): Promise<string> {
-  const request = new Request(`${process.env.WORLD_ART_MAIN_URL}/cinema?list.php`);
+  const request = new Request(`${process.env.WORLD_ART_MAIN_URL}/cinema/list.php`);
   return request.get({ query: { limit_1: (page * 25).toString(), }, });
 }
 
