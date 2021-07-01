4c56291d1a9e4c4e7fe55842ae604eaedaae5044
Nazar Bashinskiy <nazar_bashinskiy@mail.ru>
2021-07-01 20:43:26 +0300

    added job processing to index

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import 'reflect-metadata';
 import { config } from 'dotenv';
 import { createConnection } from 'typeorm';
 
+import { createJob } from './parsing/parsing.service';
+
 config();
 
 const main = async (): Promise<void> => {
   await createConnection();
+  createJob();
 }
 main().then()
