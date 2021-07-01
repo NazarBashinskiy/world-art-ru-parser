6c73a127b52a43a645aec1a6b421dcb5aac7ca0d
Nazar Bashinskiy <nazar_bashinskiy@mail.ru>
2021-07-02 00:32:06 +0300

    added parsing controller

---
+++ src/parsing/parsing.controller.ts
@@ -0,0 +1,21 @@
+import { Express, Request, Response } from 'express';
+
+import Bull from 'bull';
+
+import { getParsingStatus, startParsing } from './parsing.service';
+
+export async function parsingController (app: Express): Promise<void> {
+  app.post('/parser/start', (req: Request<null>, res: Response<Record<string, string>>, next) => {
+    try {
+      startParsing();
+      res.json({ message: 'Parsing started' });
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  app.get('/parser/status', async (req: Request<null>, res: Response<Bull.JobCounts>) => {
+    const status = await getParsingStatus();
+    res.json(status);
+  });
+}
--- src/server/controllers.ts
+++ src/server/controllers.ts
@@ -2,8 +2,10 @@ import { Express } from 'express';
 
 import { glossaryController } from '../glossaries/glossary.controller';
 import { movieController } from '../movies/movie.controller';
+import { parsingController } from '../parsing/parsing.controller';
 
 export async function controllers(app: Express): Promise<void> {
   await glossaryController(app);
   await movieController(app);
+  await parsingController(app);
 }
