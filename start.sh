2c995650ea8a8e14a7022f0d086ff99698082d08
Nazar Bashinskiy <nazar_bashinskiy@mail.ru>
2021-07-02 08:55:38 +0300

    added migrations to start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@ docker-compose up -d postgres
 sleep 5
 docker-compose up -d redis
 sleep 5
+docker-compose run --rm world-art-ru-parser npm run migrations
+sleep 5
 docker-compose up --build world-art-ru-parser
